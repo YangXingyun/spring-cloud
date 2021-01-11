25fc47d0691a5b8563ce7d4e02535fb359ed17f2
Yangxingyun <1240648933@qq.com>
2021-01-11 17:57:55 +0800

    this is yangxingyun first commit

---
+++ src/main/java/cn/kgc/yang.java
@@ -0,0 +1,14 @@
+package cn.kgc;
+
+/**
+ * @author 杨幸运
+ * @create 2021/1/11
+ */
+public class yang {
+    public static void main(String[] args) {
+        System.out.println("my name is yang x ing yun");
+        System.out.println("my name is yang x ing yun");
+        System.out.println("my name is yang x ing yun");
+        System.out.println("my name is yang x ing yun");
+    }
+}
